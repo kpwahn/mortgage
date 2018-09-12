bb636a6ee1bdcf3436357d79ee54cf3fb046b0d5
Kendall Wahnschaffe <kendall.wahnschaffe@usaa.com>
2018-09-12 16:31:46 -0500

    initial commit

---
+++ src/components/Mortgage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Mortgage extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        )
+    }
+}
+
+export default Mortgage;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devServer: {
+    stats: 'errors-only',
+    historyApiFallback: true
+  },
+  entry: './src/index.js',
+  output: {
+      path: path.resolve('dist'),
+      filename: 'bundle.js',
+      publicPath: '/'
+  },
+  mode: 'development',
+  module: {
+      rules: [
+          {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+          {test: /\.css$/, loader: 'style-loader'},
+          {test: /\.css$/, loader: 'css-loader'},
+          {test: /\.(png|jpg|gif)$/, loader: 'url-loader'}
+      ]
+  },
+  // Remove this for info about perforamce(i.e bundle size)
+  performance : {
+    hints : false
+  },
+  watchOptions: {
+    poll: true
+  }
+}
